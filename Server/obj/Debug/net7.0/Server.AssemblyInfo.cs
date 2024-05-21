8821dcc324a4553bee942b8ee43830f12f8ff46b
friedmanrivka <hand1c26@mbjcomp.org.il>
2024-05-21 17:03:11 +0300

    fix the conection string in json file now the add is working in the controller

--- Server/obj/Debug/net7.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net7.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f11818189949d09569c6ea71d71d2ebc1176fb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6251511605e344fa1f0d30e9e8d96169111c57f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
